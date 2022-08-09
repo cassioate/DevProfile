6e48da05fd0ab2ae678c2336db27a50f944fd133
Cassio <cassioate@gmail.com>
2022-08-09 15:58:11 -0300

    feat: start with styled components in the page/home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Container, Title} from './home-styles'
+
+export const Home = () => {
+  return (
+    <Container>
+      <Title>Teste</Title>
+    </Container>
+  );
+}
