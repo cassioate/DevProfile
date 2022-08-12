f18f9d521b3fd2ae3ac4834e4f1cdc7960711436
Cassio <cassioate@gmail.com>
2022-08-12 02:54:43 -0300

    feat: because of the differente behavior in the IOS, was necessary to inserted the KeyboardAvoidingView component covering all of the SignIN Page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
+import { KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
 import { ButtonStyled } from '../../components/form/button/button';
 import { Input } from '../../components/form/Input/input';
 import {
@@ -20,31 +20,38 @@ export const SignIn = () => {
   return (
     // <> <- is empty because the react not accept two root elements, só i need use this
     <>
-      <ScrollView
-        //KeyboardShouldPersistTaps="handled" means that when i click outside of the input, the keyboard will be dismissed
-        keyboardShouldPersistTaps="handled"
-        //ContetContainerStyle is used to set the padding of the ScrollView
-        contentContainerStyle={{ flex: 1 }}
+      {/* this keyboard is used because in IOS the behavior is different to Android, so this is needed to make the same behavior */}
+      <KeyboardAvoidingView
+        enabled
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
       >
-        <Container>
-          <Content>
-            <Logo source={ImageLogo}></Logo>
-            <Title>Logon</Title>
-            <Input placeholder="Email"></Input>
-            <Input placeholder="Password"></Input>
-            <ButtonStyled title="Login"></ButtonStyled>
+        <ScrollView
+          //KeyboardShouldPersistTaps="handled" means that when i click outside of the input, the keyboard will be dismissed
+          keyboardShouldPersistTaps="handled"
+          //ContetContainerStyle is used to set the padding of the ScrollView
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Content>
+              <Logo source={ImageLogo}></Logo>
+              <Title>Logon</Title>
+              <Input placeholder="Email"></Input>
+              <Input placeholder="Password"></Input>
+              <ButtonStyled title="Login"></ButtonStyled>
 
-            <ForgotPasswordButton>
-              <ForgotPasswordText>Forgot your password?</ForgotPasswordText>
-            </ForgotPasswordButton>
-          </Content>
-        </Container>
-      </ScrollView>
+              <ForgotPasswordButton>
+                <ForgotPasswordText>Forgot your password?</ForgotPasswordText>
+              </ForgotPasswordButton>
+            </Content>
+          </Container>
+        </ScrollView>
 
-      <CreateAccount>
-        <Icon name="log-in" />
-        <CreateAccountText>Create an account</CreateAccountText>
-      </CreateAccount>
+        <CreateAccount>
+          <Icon name="log-in" />
+          <CreateAccountText>Create an account</CreateAccountText>
+        </CreateAccount>
+      </KeyboardAvoidingView>
     </>
   );
 };
